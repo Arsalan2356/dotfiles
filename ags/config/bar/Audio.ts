4512daf1fb17a54ab3f43720357c46cdd927b0c2
Arsalan <khalidarsalan106@gmail.com>
2024-12-01 01:49:05 -0500

    Added title functionality, ability to play/pause/previous/next and switch active audio
    Can now see title of active audio stream
    Can play/pause active audio stream
    Can go to previous/next track of active audio stream
    Can switch between active audio streams

--- ags/config/bar/Audio.ts
+++ ags/config/bar/Audio.ts
@@ -7,43 +7,75 @@
 
 const currplayer = Variable(0)
 const clients = Variable([], {
-  poll: [1000, () => {
+  poll: [380, () => {
     const q = Utils.exec("playerctl -l").split("\n")
     return q
   }]
 })
+const ispaused = Variable(true, {
+  poll: [1000, () => {
+    if (currplayer != undefined) {
+      const ind = currplayer.getValue()
+      if (clients != undefined) {
+        const k = Utils.exec(`playerctl status -p ${clients.getValue()[ind]}`)
+        if (k.includes("Playing")) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }]
+})
 
 export default () => Widget.Box({
-  spacing: 10,
+  spacing: 14,
   css: 'padding-right: 5px',
   children: [
-    Widget.Label({
-      label: currplayer.bind().as(v => {
-        const name = clients.getValue()[v]
-        const i = name.indexOf(".")
-        return i != -1 ? name.substring(0, i) : name
-      })
+    Widget.EventBox({
+      child: Widget.Label({
+        label: clients.bind().as(v => {
+          const ind = currplayer.getValue()
+          const name = v[ind >= v.length - 1 ? v.length - 1 : ind]
+          const i = name.indexOf(".")
+          return i != -1 ? name.substring(0, i) : name
+        })
+      }),
+      on_scroll_up: (_) => {
+        const oldval = currplayer.getValue()
+        const v = clients.getValue()
+        currplayer.setValue(oldval <= 0 ? v.length - 1 : oldval - 1)
+      },
+      on_scroll_down: (_) => {
+        const oldval = currplayer.getValue()
+        const v = clients.getValue()
+        currplayer.setValue(oldval >= v.length - 1 ? 0 : oldval + 1)
+      },
     }),
     Widget.CenterBox({
-      spacing: 16,
+      spacing: 21,
       start_widget: Widget.EventBox({
         child: Widget.Icon({
           icon: `${App.configDir}/style/assets/previous.svg`,
-        })
+        }),
+        on_primary_click: () => { Utils.execAsync(`playerctl previous -p ${clients.getValue()[currplayer.getValue()]}`)}
       }),
       center_widget: Widget.EventBox({
         child: Widget.Icon({
-          icon: `${App.configDir}/style/assets/play.svg`,
-        })
+          icon: ispaused.bind().as(v => {
+            return `${App.configDir}/style/assets/${v ? "play" : "pause"}.svg`
+          })
+        }),
+        on_primary_click: () => { Utils.execAsync(`playerctl play-pause -p ${clients.getValue()[currplayer.getValue()]}`)}
       }),
       end_widget: Widget.EventBox({
         child: Widget.Icon({
           icon: `${App.configDir}/style/assets/next.svg`,
-        })
+        }),
+        on_primary_click: () => { Utils.execAsync(`playerctl next -p ${clients.getValue()[currplayer.getValue()]}`)}
       })
     }),
     Widget.Label({
-      label : "|",
+      label: "|",
     })
   ]
 })
