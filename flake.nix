6abaa83a154c1acd2242f531ca7a18454bb3ca92
Arsalan <khalidarsalan106@gmail.com>
2024-12-29 18:53:49 +0400

    Add TODO for nixpkgs update

--- flake.nix
+++ flake.nix
@@ -2,6 +2,8 @@
   description = "Nixos config flake";
 
   inputs = {
+    # TODO: update to specific commit where rocm-llvm-libc still works
+    # maybe add bleeding edge nixpkgs (master)
     nixpkgs = {
 	url = "github:nixos/nixpkgs/nixos-unstable";
     };
