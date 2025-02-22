b3ca58aac34e59290935ec05f3416c48ac9e1eef
Arsalan <khalidarsalan106@gmail.com>
2025-02-22 18:33:43 -0500

    Add custom vesktop desktop entry minimized with xwayland

--- home.nix
+++ home.nix
@@ -401,6 +401,24 @@ pkgs.mkShell {
     "$HOME/jdks/jdk/bin"
   ];
 
+  xdg.desktopEntries = {
+    "vesktop" = {
+      categories = ["Network" "InstantMessaging" "Chat"];
+      exec = "vesktop %U --start-minimized --ozone-platform=x11";
+      genericName = "Internet Messenger";
+      icon = "vesktop";
+      settings = {
+	Keywords = "discord;vencord;electron;chat";
+	StartupWMClass = "Vesktop";
+	Version = "1.4";
+      };
+      name = "Vesktop";
+      type = "Application";
+    };
+  };
+
+
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
