86fa6d311f25fb7cc13663f0ca35f3ed4bbca5d5
Arsalan <khalidarsalan106@gmail.com>
2025-03-08 17:32:41 -0500

    Remove old hyprland update script

--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-branch=$(curl --silent "https://api.github.com/repos/hyprwm/Hyprland/releases/latest" | jq -r '.target_commitish')
-
-sed -i "s/hyprland.url.*/hyprland.url = \"github:hyprwm\/Hyprland\/$branch\";/" flake.nix
-
-commit=$(curl --silent "https://api.github.com/repos/outfoxxed/hy3/tags?per_page=1" | jq -r '.[0].commit.sha')
-
-sed -i "/hy3/{n;s/url.*/url = \"github:outfoxxed\/hy3\/$commit\";/}" flake.nix
